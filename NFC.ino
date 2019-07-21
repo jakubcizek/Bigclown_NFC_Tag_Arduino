235a2979bbc38cd78d9812c7ea5e1cfd273641c5
Jakub Čížek <cizekuba@gmail.com>
2019-07-21 15:25:36 +0200

    Popis kodu projektu Arduina

--- NFC.ino
+++ NFC.ino
@@ -2,14 +2,20 @@
 
 // NFC cip
 bc_tag_nfc_t nfc;
+
+// Je NFC cip v poradku inciializovany?
 bool nfc_ok = false;
+
+// Zprava ze seriove linky
 String zprava;
 
+// Pomocny enum pro typ NDEF zpravy (URL nebo text)
 typedef enum {
   URI,
   PLAINTEXT
 } ndef_typ_t;
 
+// Funkce pro ulzoeni NDEF zpravy do EEPROM pameti NFC cipu
 bool ulozDoNFC(char* txt, ndef_typ_t typ) {
   bc_tag_nfc_ndef_t zprava;
   bc_tag_nfc_ndef_init(&zprava);
@@ -27,11 +33,14 @@ bool ulozDoNFC(char* txt, ndef_typ_t typ) {
   }
 }
 
+// Hlavni funkce Arduina; spusti se po startu
 void setup() {
+  // Nastartovani seriove linky
   Serial.begin(115200);
-  zprava.reserve(500);
+  // Rezervace pameti pro dynamicky objekt String (vytvari se v heap)
+  zprava.reserve(200);
 
-  // Incializace I2C NFC cipu na adrese 0x08
+  // Incializace BigClown NFC Tag na I2C adrese 0x08
   if ((nfc_ok = bc_tag_nfc_init(&nfc, 0x08)) == false) {
     Serial.println("Nepodarilo se inicializovat NFC cip :-(");
   }
@@ -40,18 +49,33 @@ void setup() {
   }
 }
 
+// Smycka loop se spusti po funkci setup a opakuje se stale dokola
 void loop() {
+  // Pokud na seriove lince dorazila nejaka data
+  /*
+   * Predpokladejme format zpravy:
+   * URL: U xxxxxxxxx
+   * Prosty text: T xxxxxxxxxx
+  */
   while (Serial.available()) {
+    // Precti je jako text az do zalomeni radku
     zprava = Serial.readStringUntil('\n');
+    // Smaz ze zpravy pripadne neviditelne znaky (\r apod.)
     zprava.trim();
+    
+    // Pokud NFC neni pripraveny, napis do seriove linky varovani a ukonci smycku
     if(!nfc_ok){
-      Serial.println("NNFC cip neni inicializovany!");
+      Serial.println("NFC cip neni inicializovany!");
       break;
     }
+    
+    // Pokud je prvni znak zpravy U, jedna se o WWW adresu
     if (zprava[0] == 'U') {
+      // Odstran prvni dva znaky a pro kontrolu vypis do seriove lniky, co ulozim do EEPROM NFC cipu
       zprava.remove(0, 2);
       Serial.print("Vytvarim NDEF zpravu s URL: ");
       Serial.println(zprava);
+      // Pokus se URL ulozit do EEPROM NFC cipu jako URI NDEF zpravu
       if (ulozDoNFC(zprava.c_str(), URI)) {
         Serial.println("Zapsano do NFC cipu!");
       }
@@ -59,10 +83,12 @@ void loop() {
         Serial.println("Nelze zapsat do NFC cipu!");
       }
     }
+    // Pokud je prvni znak zpravy T, jedna se o plaintext zpravu
     else if (zprava[0] == 'T') {
       zprava.remove(0, 2);
       Serial.print("Vytvarim NDEF zpravu: ");
       Serial.println(zprava);
+      // Pokud se prosty text ulozit do EEPROM NFC cipu jako NDEF zpravu s prostym textem
       if (ulozDoNFC(zprava.c_str(), PLAINTEXT)) {
         Serial.println("Zapsano do NFC cipu!");
       }
